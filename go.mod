7dc261c9a5b576902df39971c8415b3307b45655
Kelwing <aquabug@gmail.com>
2023-03-31 13:01:51 -0400

    license + mod changes

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Project-Sparrow/snowflake
+module wumpgo.dev/snowflake
 
-go 1.19
+go 1.20
--- snowflake.go
+++ snowflake.go
@@ -1,6 +1,7 @@
 // MIT License
 
 // Copyright (c) 2022 Project-Sparrow
+// Copyright (c) 2023 Kelwing <kelwing@kelnet.org>
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
